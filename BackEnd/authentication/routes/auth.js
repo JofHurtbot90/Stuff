6086181f3b267da00d985ef74200115f78acd02d
Josh Hurtado <joshhurtado@Joshs-MacBook-Pro.local>
2019-02-25 14:45:18 -0500

    update

--- BackEnd/authentication/routes/auth.js
+++ BackEnd/authentication/routes/auth.js
@@ -3,3 +3,26 @@ const User = require('../models/user');
 const authRouter = express.Router();
 const jwt = require('jsonwebtoken');
 
+authRouter.post('signup', (req, res, next)=>{
+    User.findOne({username: req.body.username}, (err, existingUser){
+        if (err) {
+            res.status(500);
+            return next(err);
+        }
+        if (existingUser !== null){
+            res.status(400);
+            return next(new Error('That username already exists.'));
+        }
+        const newUser = new User(req.body);
+         newUser.save((err, user)=>{
+             if (err){
+                 res.status(500);
+                 return next(err);
+             }
+         });
+    });
+});
+
+authRouter.post('/login', (req, res, next)=>{
+    User.findOne({username: })
+})
