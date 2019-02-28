e9b1d4091454710e9f9e7eded2b20aa513d3e4ec
Josh Hurtado <joshhurtado@Joshs-MacBook-Pro.local>
2019-02-28 18:30:11 -0500

    update

--- react_authenticate/src/server/App.js
+++ react_authenticate/src/server/App.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const PORT = process.env.PORT || 5000;
 const app = express();
 const expressJwt = require('express-jwt');
 require('dotenv').config();
@@ -12,26 +12,26 @@ app.use(bodyParser.json());
 app.use('/api', expressJwt({secret: process.env.SECRET}));
 
 mongoose.set('useCreateIndex', true);
-mongoose.connect('mongodb://localhost:27017/authentication',
+mongoose.connect('mongodb://localhost:27017/react-authentication',
     {userNewUrlParser: true},
     (err) => {
-        if (err) throw err;
+      if (err) throw err;
         console.log('Connected to the database');
     }
-); 
+);
 
 app.use('/todo', require('./routes/todo'));
 app.use('/auth', require('./routes/auth'));
 app.use('/api/todo', require('./routes/todo'));
 
-app.use((err, req, res, next) =>{
+app.use((err, req, res, next)=>{
     console.error(err);
-    if (err.name === 'UnauthorizedError') {
+    if(err.name === 'UnauthorizedError') {
         res.status(err.status);
     }
     return res.send({message: err.message});
 });
 
-app.listen(PORT, () =>{
+app.listen(PORT, ()=>{
     console.log(`[+] Starting server on port ${PORT}`);
 });
