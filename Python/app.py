d2b00cd275d4215f5d061534b39fd36d86948306
Josh Hurtado <joshhurtado@Joshs-MacBook-Pro.local>
2019-06-07 13:50:17 -0400

    update

--- Python/app.py
+++ Python/app.py
@@ -1,50 +1,79 @@
-num = 5
-
-if num > 6:
-    print('number is less than 6')
-elif num < 4:
-    print('number is lesser than 4')
-else:
-    print('the number is 5')
-
-
-age = 21
-
-message = 'You are eligible' if age >= 18 else 'you are not eligible'
-
-print(message)
-
-for x in 'PYTHON':
-    print(x)
-
-for num in range(3):
-    print(num)
-
-number = 100
-while number >= 1:
-    print(number)
-    number //= 2
-
-x = 100
-
-for number in range(3):
-    if x > 1:
-        print('positive')
-        break
-    
-    else:
-        print('negative')
-
-for x in range(5):
-    for y in range(3):
-        print(x,y)
-
 blockchain = []
 
-def add_list():
-    blockchain.append(3.2)
-    print(blockchain)
+def get_last_value():
+   
+   """extracting the last element of the blockchain list"""
 
-add_list()
-add_list()
-add_list()
+   return(blockchain[-1])
+
+def add_value(transaction_amount, last_transaction=[-1]):
+
+    blockchain.append([last_transaction, transaction_amount])
+
+def get_transaction_value():
+    
+    user_value = float(input('Enter your transaction amount'))
+
+    return user_value
+
+
+def get_user_choice():
+    
+    user_input = input('Please give your choice here:')
+    
+    return user_input
+
+def print_block():
+
+    for block in blockchain:
+
+        print('Here is your block')
+        print(block)
+
+def verify_chain():
+
+    index = 0
+
+    valid = True
+
+    
+    for block in blockchain:
+        
+      if index == 0:
+        index += 1
+
+        continue
+
+      elif block[0] == blockchain[index - 1]:
+
+          valid = True
+        
+      else:
+         
+          valid = False
+
+          break
+
+      index += 1
+
+    return valid
+
+tx_amount = get_transaction_value()
+
+add_value(tx_amount)
+
+while True:
+
+  print('Choose an option')
+  print('Choose 1 for adding a new transaction')
+  print('Choose 2 for printing the blockchain')
+  print('Choose 3 if you want to manipulate the data')
+  print('Choose anything else if you want to quit')
+
+user_choice = get_user_choice()
+
+if user_choice == 1:
+
+    tx_amount = get_transaction_value()
+
+    add_value(tx_amount, get_last_value())
