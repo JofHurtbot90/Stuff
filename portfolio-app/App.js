3770a2300d941f93860d44bec04d0180e312d647
Josh Hurtado <joshhurtado@Joshs-MacBook-Pro.local>
2020-04-23 16:20:17 -0400

    update

--- portfolio-app/App.js
+++ portfolio-app/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './styles.css';
+
 
 
 
@@ -9,14 +9,22 @@ function App() {
 
     <div>
 
-      <h1>Joshua Hurtado</h1>
+      <h1 class=
+      "position absolute width 303px height 152px left 127px top 215px font-family Comfortaa font-size 68px">Joshua Hurtado</h1>
 
-    <p> 
+    <p class=
+    "position absolute width 791px height 240px left 127px top 448px font-family Comfortaa font-size 36px color #6D6D6D">
+    
       I am a Javascript Developer and Backend Developer that builds from the bottom up. 
-      I start from the backend that include authentication, servers, hosts, RestAPI's and middleware 
+      I start from the backend that include authentication, servers, hosts, RestAPIs and middleware 
       then work my way to the frontend. </p>
 
-      <h2>Skills</h2>
+      <h2 class=
+      "position absolute width 114px height 47px left 120px top 783px font-weight bold font-size 42px " 
+>Skills class=
+"position absolute width 47px left 121px top 1226px font-family Comfortaa font-weight bold font-size 42px "</h2>
+ <div class=
+ "position absolute width 166px height 192px left 162px top 893px font-family Comfortaa font-size 30px color: #6D6D6D">
 
         <ol>Javascript</ol>
         <ol>React</ol>
@@ -30,12 +38,13 @@ function App() {
         <ol>Token Authentication</ol>
         <ol>HTML</ol>
         <ol>CSS</ol>
-
+ </div>
+       
       <h2>Projects</h2>
-      
+
       </div>
     
   )
-}
+};
 
 export default App;
