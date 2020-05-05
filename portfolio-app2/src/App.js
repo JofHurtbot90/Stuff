20f2522f9665de40ac3ce2c82b94b6fb22cf4208
Josh Hurtado <joshhurtado@Joshs-MacBook-Pro.local>
2020-05-05 14:24:24 -0400

    update

--- portfolio-app2/src/App.js
+++ portfolio-app2/src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   
+   return (
+
+    <div>
+
+      <h1 class=
+      "position absolute width 303px height 152px left 127px top 215px font-family Comfortaa font-size 68px">Joshua Hurtado</h1>
+
+    <p class=
+    "position absolute width 791px height 240px left 127px top 448px font-family Comfortaa font-size 36px color #6D6D6D">
+    
+      I am a Javascript Developer and Backend Developer that builds from the bottom up. 
+      I start from the backend that include authentication, servers, hosts, RestAPIs and middleware 
+      then work my way to the frontend. </p>
+
+      <h2 class=
+      "position absolute width 114px height 47px left 120px top 783px font-weight bold font-size 42px " 
+>Skills class=
+"position absolute width 47px left 121px top 1226px font-family Comfortaa font-weight bold font-size 42px "</h2>
+ <div class=
+ "position absolute width 166px height 192px left 162px top 893px font-family Comfortaa font-size 30px color: #6D6D6D">
+
+        <ol>Javascript</ol>
+        <ol>React</ol>
+        <ol>MongoDB</ol>
+        <ol>Node.js</ol>
+        <ol>Mongoose</ol>
+        <ol>Express</ol>
+        <ol>Github</ol>
+        <ol>Git</ol>
+        <ol>OAuth</ol>
+        <ol>Token Authentication</ol>
+        <ol>HTML</ol>
+        <ol>CSS</ol>
+ </div>
+       
+      <h2>Projects</h2>
+
+      </div>
+    
+  )
+};
 
 export default App;
