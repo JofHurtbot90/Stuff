5f5527c5b744a88f54d92f30ea5e4b1215b53e14
Josh Hurtado <joshhurtado@Joshs-MacBook-Pro.local>
2020-05-22 14:42:26 -0400

    update

--- portfolio-app2/src/App.js
+++ portfolio-app2/src/App.js
@@ -1,50 +1,50 @@
 import React from 'react';
 
-
-
+//font - sans text - xl text - grey - 800 text - left//
+//font - sans text - md text - black text - left
+//font - sans text - lg text - grey - 800 text - left
+//font - sans text - md text - black text - left
 
 function App() {
    
-   return (
-
-    <div>
-
-      <h1 class=
-      "position absolute width 303px height 152px left 127px top 215px font-family Comfortaa font-size 68px">Joshua Hurtado</h1>
-
-    <p class=
-    "position absolute width 791px height 240px left 127px top 448px font-family Comfortaa font-size 36px color #6D6D6D">
+   return (   
     
-      I am a Javascript Developer and Backend Developer that builds from the bottom up. 
-      I start from the backend that include authentication, servers, hosts, RestAPIs and middleware 
-      then work my way to the frontend. </p>
+  <div class='container'>
 
-      <h2 class=
-      "position absolute width 114px height 47px left 120px top 783px font-weight bold font-size 42px " 
->Skills class=
-"position absolute width 47px left 121px top 1226px font-family Comfortaa font-weight bold font-size 42px "</h2>
- <div class=
- "position absolute width 166px height 192px left 162px top 893px font-family Comfortaa font-size 30px color: #6D6D6D">
+    <h1><span class='blue-text text-darken-2'> Joshua Hurtado </span></h1>
 
-        <ol>Javascript</ol>
-        <ol>React</ol>
-        <ol>MongoDB</ol>
-        <ol>Node.js</ol>
-        <ol>Mongoose</ol>
-        <ol>Express</ol>
-        <ol>Github</ol>
-        <ol>Git</ol>
-        <ol>OAuth</ol>
-        <ol>Token Authentication</ol>
-        <ol>HTML</ol>
-        <ol>CSS</ol>
- </div>
-       
-      <h2>Projects</h2>
+      <p class='flow-text'>
+        I am a Javascript Developer and Backend Developer that builds from the bottom up.
+      I start from the backend that include authentication, servers, hosts, RestAPIs and middleware
+      then work my way to the frontend. 
+      </p>
 
-      </div>
-    
+      <h2><span class='blue-text text-darken-2'> Skills </span></h2>
+
+        <ul class='collection'>
+
+        <li class='collection-item'> Javascript </li> 
+        <li class='collection-item'> React </li > 
+        <li class='collection-item'> MongoDB </li> 
+        <li class='collection-item'> Node.js </li> 
+        <li class='collection-item'> Mongoose </li> 
+        <li class='collection-item'> Express </li> 
+        <li class='collection-item'> Github </li> 
+        <li class='collection-item'> Git </li> 
+        <li class='collection-item'> OAuth </li> 
+        <li class='collection-item'> Token Authentication </li> 
+        <li class='collection-item'> HTML </li> 
+        <li class='collection-item'> CSS </li> 
+        
+        </ul>
+
+        <h2><span class='blue-text text-darken-2'> Projects </span></h2>
+
+      
+    </div>      
+  
   )
 };
 
 export default App;
+
--- socket-app/script.js
+++ socket-app/script.js
@@ -1,7 +1,7 @@
-const socket = io('http://localhost:3000')
-const messageContainer = document.getElementById('message-container')
-const messageForm = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
+const socket = io('http://localhost:3000');
+const messageContainer = document.getElementById('message-container');
+const messageForm = document.getElementById('send-container');
+const messageInput = document.getElementById('message-input');
 
 const name = prompt('What is your name?')
 appendMessage('You joined')
@@ -9,15 +9,15 @@ socket.emit('new-user', name)
 
 socket.on('chat-message', data =>{
     appendMessage(`${data.name}: ${data.message}`)
-})
+});
 
 socket.on('user-connected', name =>{
     appendMessage(`${name} connected`)
-})
+});
 
 socket.on('user-disconnected', name =>{
     appendMessage(`${name} disconnected`)
-})
+});
 
 messageForm.addEventListener('submit', e =>{
     e.preventDefault()
@@ -25,10 +25,10 @@ messageForm.addEventListener('submit', e =>{
     appendMessage(`You ${message}`)
     socket.emit('send-chat-message', message)
     messageInput.value = ''
-})
+});
 
 function appendMessage(message) {
     const messageElement = document.createElement('div')
     messageElement.innerText = message
     messageContainer.append(messageElement)
-}
+};
